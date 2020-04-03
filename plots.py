a366ad9be49eb02c110da6b36b92ba0855c81ec2
Anthony Maylath <anthonymaylath@Anthonys-MacBook-Pro-2.local>
2020-04-02 20:28:30 -0400

    Add vol diffs

--- plots.py
+++ plots.py
@@ -2,23 +2,26 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-def plt_surf(mySurf, graph_scn):
+def plt_surf(mySurf, graph_scn,diff=False,save=False):
     '''
     Surface plot for an implied volatility surface for a particular scenario or date
     :param mySurf: VolSurf object
     :param graph_scn: Date or scenario to plot
+    :param diff: If true, use vol % differences instead of volatility quote
+    :param save: If true, save a file of the image
     :return: None
     '''
     tim = np.linspace(0.08,4,30)
     money = np.linspace(0.4,1.6,30)
     tim, money = np.meshgrid(tim,money)
     vecfunc = np.vectorize(mySurf.nwInterp)
-    ivs = vecfunc(money,tim,graph_scn)
+    ivs = vecfunc(money,tim,graph_scn,diff)
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.plot_surface(money,tim,ivs)
     ax.view_init(30,60)
-    plt.savefig(graph_scn + '.pdf')
+    if save:
+        plt.savefig(str(graph_scn) + '.pdf')
     plt.show()
     return None
 
@@ -34,3 +37,21 @@ def plt_importance(mySurf):
     plt.bar([str(x) for x in range(1,len(importance)+1)],height=importance)
     plt.savefig("factorimportance.pdf")
     plt.show()
+    return None
+
+def plt_proj_time(mySurf, pca_factors):
+    '''
+    Plot a time series of the projected factors
+    :param mySurf: Implied volatility object
+    :param pca_factors: Number of pca factors
+    :return: None
+    '''
+    proj = mySurf.proj_fact()
+    labels = ['Factor {}'.format(l+1) for l in range(np.shape(proj)[0])]
+    for i in range(pca_factors):
+        plt.plot(proj[i],label=labels[i])
+    plt.xlabel('Days')
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=4, mode="expand", borderaxespad=0.)
+    plt.show()
+    return None
